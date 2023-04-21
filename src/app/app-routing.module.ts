43522010ad1cd66aec2e845dc2383a3fbae4d388
Kriszti <letina84@gmail.com>
2023-04-21 15:48:42 +0200

    CREATE and ADD DESIGN -- categories component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoriesComponent } from './categories/categories.component';
 
+const routes: Routes = [
+  { path: '', component: DashboardComponent},
+  { path: 'categories', component: CategoriesComponent}
+];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AngularFireModule} from '@angular/fire/compat';
-//import firebase from 'firebase/compat/app';
 import { environment } from 'src/environments/environment.development';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { AppRoutingModuleTsComponent } from './app-routing.module.ts/app-routing.module.ts.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -18,14 +15,12 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DashboardComponent,
-    CategoriesComponent,
-    AppRoutingModuleTsComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
