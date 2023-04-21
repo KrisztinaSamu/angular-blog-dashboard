0026ac953fda1fffb04a36223df76a065471a27a
Kriszti <letina84@gmail.com>
2023-04-21 11:58:30 +0200

    CREATE and ADD categories and AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { AppRoutingModuleTsComponent } from './app-routing.module.ts/app-routing.module.ts.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    CategoriesComponent,
+    AppRoutingModuleTsComponent
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
